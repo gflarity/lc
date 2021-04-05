0d4be7f721c4b9b4ea802d517e23e416dcb5ca6e
Geoffrey Flarity <geoff.flarity@gmail.com>
2021-04-05 13:27:54 -0400

    valid palindrome

---
+++ valid-palindrome/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func isPalindrome(s string) bool {
+	// NOTE: It would be more computationally effecient to check
+	// characters/case char by char, but it would also be more complex
+	// and error prone code, so I'd stick with this unless there there was
+	// a requirement.
+	// get rid of non alpha numerics
+	var re = regexp.MustCompile("[^a-zA-Z0-9]")
+	s = re.ReplaceAllString(s, "")
+	s = strings.ToLower(s)
+
+	i := 0
+	j := len(s) - 1
+	for i <= j {
+		if s[i] != s[j] {
+			return false
+		}
+		i++
+		j--
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(isPalindrome("a 1 a"))
+}
