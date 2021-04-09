1312c13881ca4b4432f40555044323ab720376d9
Geoffrey Flarity <geoff.flarity@gmail.com>
2021-04-09 09:05:39 -0400

    add go test

--- best-time-to-buy-and-sell-stock/main.go
+++ best-time-to-buy-and-sell-stock/main.go
@@ -26,18 +26,25 @@ func maxProfitBrute(prices []int) int {
 }
 
 func maxProfit(prices []int) int {
-
 	// edge cases, 0 or 1 trade
 	if len(prices) == 0 || len(prices) == 1 {
 		return 0
 	}
 
-	// track the max profits
-	mp := 0
+	b := 0
+	mp := prices[1] - prices[b]
+	for i := 1; i < len(prices); i++ {
+		if prices[i] < prices[b] {
+			b = i
+			continue
+		}
+		if prices[i] - prices[b] > mp {
+			mp = prices[i] - prices[b]
+		}
+	}
 
+	if mp < 0 {
+		return 0
+	}
 	return mp
-}
-
-func main() {
-	//	fmt.Println(maxProfit([]int{7, 1, 5, 3, 6, 4}))
 }
